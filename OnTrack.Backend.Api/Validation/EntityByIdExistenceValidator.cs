0f21faa7858bb91fe97b3dabb1e5bd6d54b553b5
Adrian Grill <agrill@topsa.pl>
2024-03-02 20:19:27 +0100

    Prepared EntityByIdExistenceValidator class for becoming entity validation base

--- OnTrack.Backend.Api/Validation/EntityByIdExistenceValidator.cs
+++ OnTrack.Backend.Api/Validation/EntityByIdExistenceValidator.cs
@@ -7,25 +7,25 @@ using OnTrack.Backend.Api.Services;
 
 namespace OnTrack.Backend.Api.Validation;
 
-public class EntityByIdExistenceValidator<TEntityId, TEntity>(IEntityAccessService<TEntity, TEntityId> entityAccessService)
+public class EntityByIdExistenceValidator<TEntityId, TEntity>(IEntityAccessService<TEntityId, TEntity> entityAccessService)
 	: IAsyncCollectionValidator<TEntityId, OneOf<TEntity, EntityIdErrorsDescription<TEntityId>>>
 	where TEntityId : IStronglyTypedId
 	where TEntity : IEntity<TEntityId>
 {
-	private const string _errorMessageTemplate = "{0} with this Id does not exist.";
+	protected const string entityIdDoesNotExistErrorMessageTemplate = "{0} with this Id does not exist.";
 
-	private readonly IEntityAccessService<TEntity, TEntityId> _entityAccessService = entityAccessService;
+	protected IEntityAccessService<TEntityId, TEntity> EntityAccessService { get; } = entityAccessService;
 
 	public async Task<OneOf<TEntity, EntityIdErrorsDescription<TEntityId>>> Validate(TEntityId item, CancellationToken cancellationToken)
 	{
-		TEntity? entity = await _entityAccessService.Find(item, cancellationToken);
+		TEntity? entity = await EntityAccessService.Find(item, cancellationToken);
 
 		cancellationToken.ThrowIfCancellationRequested();
 
 		return entity switch
 		{
 			not null => entity,
-			null => new EntityIdErrorsDescription<TEntityId>(item, [string.Format(_errorMessageTemplate, typeof(TEntity).Name)])
+			null => new EntityIdErrorsDescription<TEntityId>(item, [string.Format(entityIdDoesNotExistErrorMessageTemplate, typeof(TEntity).Name)])
 		};
 	}
 
