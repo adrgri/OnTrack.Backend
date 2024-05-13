2d2a635f66f3ce3b130d378964aba58410163e91
Adrian Grill <agrill@topsa.pl>
2024-05-13 17:13:29 +0200

    Enhanced Entity record class with data annotations

--- OnTrack.Backend.Api/Models/Entity.cs
+++ OnTrack.Backend.Api/Models/Entity.cs
@@ -1,7 +1,14 @@
-﻿namespace OnTrack.Backend.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnTrack.Backend.Api.Models;
 
 public record class Entity<TEntityId> : IEntity<TEntityId>
 	where TEntityId : IStronglyTypedId, new()
 {
+	[Key]
+#if ApplicationGeneratedGuidIdsAllowed == false
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+#endif
 	public TEntityId Id { get; set; }
 }
