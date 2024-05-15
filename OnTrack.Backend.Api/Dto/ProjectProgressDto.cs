f77626c5027f853ba7c3f257a4da2c02ba822cff
Adrian Grill <agrill@topsa.pl>
2024-05-15 13:38:59 +0200

    Added Task and Project concrete progress dtos

---
+++ OnTrack.Backend.Api/Dto/ProjectProgressDto.cs
@@ -0,0 +1,5 @@
+﻿using OnTrack.Backend.Api.Models;
+
+namespace OnTrack.Backend.Api.Dto;
+
+public sealed record class ProjectProgressDto(ProjectId Id, Progress Progress) : ProgressDto<ProjectId>(Id, Progress);
---
+++ OnTrack.Backend.Api/Dto/TaskProgressDto.cs
@@ -0,0 +1,5 @@
+﻿using OnTrack.Backend.Api.Models;
+
+namespace OnTrack.Backend.Api.Dto;
+
+public sealed record class TaskProgressDto(TaskId Id, Progress Progress) : ProgressDto<TaskId>(Id, Progress);
