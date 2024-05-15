ffe0e597ddda84027b61e9701b6d9cb476441b07
Adrian Grill <agrill@topsa.pl>
2024-05-15 13:36:57 +0200

    Added ProgressDto base record class

---
+++ OnTrack.Backend.Api/Dto/ProgressDto.cs
@@ -0,0 +1,10 @@
+﻿using OnTrack.Backend.Api.Models;
+
+namespace OnTrack.Backend.Api.Dto;
+
+public record class ProgressDto<TEntityId>(TEntityId Id, Progress Progress) : IDtoWithId<TEntityId>
+	where TEntityId : IStronglyTypedId
+{
+	public TEntityId Id { get; set; } = Id;
+	public Progress Progress { get; set; } = Progress;
+}
