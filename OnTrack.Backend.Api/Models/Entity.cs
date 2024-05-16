32a30da79431844a6278eb2666dac5098a246113
Adrian Grill <agrill@topsa.pl>
2024-05-16 04:30:07 +0200

    Moved all attributes from Entity class to IEntity interface, where they should be

--- OnTrack.Backend.Api/Models/Entity.cs
+++ OnTrack.Backend.Api/Models/Entity.cs
@@ -1,14 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace OnTrack.Backend.Api.Models;
+﻿namespace OnTrack.Backend.Api.Models;
 
 public record class Entity<TEntityId> : IEntity<TEntityId>
 	where TEntityId : IStronglyTypedId, new()
 {
-	[Key]
-#if ApplicationGeneratedGuidIdsAllowed == false
-	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-#endif
 	public TEntityId Id { get; set; }
 }
--- OnTrack.Backend.Api/Models/IEntity.cs
+++ OnTrack.Backend.Api/Models/IEntity.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnTrack.Backend.Api.Models;
 
@@ -6,5 +7,8 @@ public interface IEntity<TEntityId>
 	where TEntityId : IStronglyTypedId
 {
 	[Key]
+#if ApplicationGeneratedGuidIdsAllowed == false
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+#endif
 	TEntityId Id { get; set; }
 }
