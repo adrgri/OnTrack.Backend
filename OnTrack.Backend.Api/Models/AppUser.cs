cdf3b0bc9c396d0a2078f53fa9e5c24ed3f21011
Adrian Grill <agrill@topsa.pl>
2024-05-20 01:57:26 +0200

    Deleted validation attributes from the model, since the model should not know whether the data is valid or not

--- OnTrack.Backend.Api/Models/AppUser.cs
+++ OnTrack.Backend.Api/Models/AppUser.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 using OnTrack.Backend.Api.Infrastructure.DataAccess;
@@ -10,15 +8,12 @@ namespace OnTrack.Backend.Api.Models;
 [EntityTypeConfiguration<AppUserConfiguration, AppUser>]
 public sealed class AppUser : IdentityUser<IdentitySystemObjectId>, IEntity<IdentitySystemObjectId>
 {
-	[Length(2, 20)]
 	[ProtectedPersonalData]
 	public string? FirstName { get; set; }
 
-	[Length(0, 40)]
 	[ProtectedPersonalData]
 	public string? LastName { get; set; }
 
-	[Length(0, 1_000)]
 	[ProtectedPersonalData]
 	public string? Bio { get; set; }
 
