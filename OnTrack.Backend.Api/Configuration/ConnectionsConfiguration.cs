35824b21754c1feabe3d1753fee2887cc2b82dc8
Adrian Grill <agrill@topsa.pl>
2024-05-24 20:20:07 +0200

    Added a new setting to the connections configuration, which specifies what SQL Server type the user wants to use

--- OnTrack.Backend.Api/Configuration/ConnectionsConfiguration.cs
+++ OnTrack.Backend.Api/Configuration/ConnectionsConfiguration.cs
@@ -8,6 +8,9 @@ public sealed class ConnectionsConfiguration : IOptionsSection
 	public static string SectionKey => "ConnectionStrings";
 
 	[Required]
+	public SqlServerType SqlServerType { get; set; }
+
+	[Required]
 	[JsonIgnore]
 	public string SqlDatabase { get; set; }
 }
